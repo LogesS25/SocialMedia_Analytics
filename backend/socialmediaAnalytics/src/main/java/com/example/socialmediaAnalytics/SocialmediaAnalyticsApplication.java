638953f79a7126cd65f08c1923c0160c4230828d
Loges <sivalog25@gmail.com>
2024-07-18 22:32:51 +0530

    backend-setup

---
+++ backend/socialmediaAnalytics/src/main/java/com/example/socialmediaAnalytics/SocialmediaAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialmediaAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaAnalyticsApplication.class, args);
+	}
+
+}
